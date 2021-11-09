04ba80c4deff93bf2d4149f9dff85a79bb26b59e
EdwaardG <eenjosa@gmail.com>
2021-11-09 08:35:09 +0800

    share code on github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import data from  './data';
+import {BrowserRouter, Route} from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/Homescreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
     <header className="row">
         <div>
@@ -14,43 +18,15 @@ function App() {
         </div>
     </header>
     <main>
-        <div className="row center">
-          {
-            data.products.map(product => (
-              <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                  <img className="medium" 
-                  src={product.image} 
-                  alt={product.name} />
-              </a>
-              <div className="card-body">
-              <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                      <span><i className="fa fa-star"></i></span>
-                      <span><i className="fa fa-star"></i></span>
-                      <span><i className="fa fa-star"></i></span>
-                      <span><i className="fa fa-star"></i></span>
-                      <span><i className="fa fa-star-o"></i></span>
-                  </div>
-                  <div className="price">
-                      ${product.price}
-                  </div>
-              </div>
-          </div>
-
-            ))
-          }
-            
-
-           
-        </div>
+    <Route path="/cart/:id?" component={CartScreen}></Route>
+    <Route path="/product/:id" component={ProductScreen}></Route>
+    <Route path="/" component={HomeScreen} exact></Route>
     </main>
     <footer className="row center">
         All Rights Reserved
     </footer>
 </div>
+</BrowserRouter>
   );
 }
 
