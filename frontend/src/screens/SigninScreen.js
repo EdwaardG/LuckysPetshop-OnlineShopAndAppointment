358f5775ba8f953b451a0d00906d283f9f44911e
EdwaardG <eenjosa@gmail.com>
2021-11-22 21:00:24 +0800

    share code on github

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -65,7 +65,10 @@ export default function SigninScreen(props) {
         <div>
           <label />
           <div>
-            New customer? <Link to="/register">Create your account</Link>
+          New customer?{' '}
+            <Link to={`/register?redirect=${redirect}`}>
+              Create your account
+            </Link>
           </div>
         </div>
       </form>
