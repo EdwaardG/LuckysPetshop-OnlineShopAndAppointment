da4cb08aa39c266f056239940d3581c3729e2760
EdwaardG <eenjosa@gmail.com>
2021-11-28 22:34:11 +0800

    share code on github 1128

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -5,6 +5,15 @@ import express from 'express';
 
 const orderRouter = express.Router();
 
+orderRouter.get(
+  '/mine',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
+
 orderRouter.post(
     '/',
     isAuth,
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { createStore, compose, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer, } from './reducers/orderReducers';
+import { orderCreateReducer, orderDetailsReducer, orderMineListReducer, orderPayReducer, } from './reducers/orderReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import {
+    userDetailsReducer,
     userRegisterReducer,
     userSigninReducer,
+    userUpdateProfileReducer,
   } from './reducers/userReducers';
 
 const initialState = {
@@ -33,6 +35,9 @@ const reducer = combineReducers({
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
+    orderMineList: orderMineListReducer,
+    userDetails: userDetailsReducer,
+   userUpdateProfile: userUpdateProfileReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
