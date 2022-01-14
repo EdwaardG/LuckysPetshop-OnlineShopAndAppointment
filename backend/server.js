613bd5ed40394bcf3259546f06632f1fa0d9c7d5
clairefrancisco <clairefrancisco1@gmail.com>
2022-01-15 00:37:28 +0800

    added exp

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -88,15 +89,15 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-// const __dirname = path.resolve();
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-// );
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
